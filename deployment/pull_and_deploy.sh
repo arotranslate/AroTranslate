dd54b81c71994433f7570f2fe9e0a403b922262a
snisioi <sergiu.nisioi@gmail.com>
2024-09-10 20:21:14 +0300

    :mushroom: update about link

---
+++ deployment/pull_and_deploy.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+COMPOSE_FILE="${1:-docker-compose.yml}"
+
+echo "Using ${COMPOSE_FILE}"
+
+git pull && \
+docker compose -f $COMPOSE_FILE pull && \
+#docker compose -f $COMPOSE_FILE build && \
+docker compose -f $COMPOSE_FILE up -d && \
+echo "Done!"
+#sleep 5
+#&& \
+#docker compose logs
